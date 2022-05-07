9d309a226e352d8768c85907a1452cc22db73e80
Evgeniy Antipov <evantype@gmail.com>
2022-05-07 23:58:14 +0300

    user

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Route;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->validated());
+        return new UserResource($user);
+    }
+}
