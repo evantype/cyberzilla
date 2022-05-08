a0e8c167ba87c9196f2b7fa14c0eda8dfe0527dd
Evgeniy Antipov <evantype@gmail.com>
2022-05-08 23:18:49 +0300

    заглушка для главной и подключение скриптов

---
+++ resources/js/views/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1 class="h1">Home page</h1>
+        <router-link v-if="isLoggedIn" to="/users" type="button" class="btn btn-primary">Перейти к списку пользователей</router-link>
+        <div v-else>Please do log in</div>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from "../components/HeaderComponent";
+
+export default {
+    name: "App",
+    components: {
+        HeaderComponent
+    },
+    data() {
+        return {
+            isLoggedIn: false,
+        }
+    },
+    created() {
+        if (window.Laravel.isLoggedin) {
+            this.isLoggedIn = true
+        }
+    },
+}
+</script>
