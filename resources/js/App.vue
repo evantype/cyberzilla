ece2579c5a04927ce23f04ba0457fb6be7a0e045
Evgeniy Antipov <evantype@gmail.com>
2022-05-08 23:17:24 +0300

    подключение vue

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <HeaderComponent/>
+        <div class="container">
+            <br/>
+            <router-view/>
+        </div>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent";
+export default {
+    name: "App",
+    components: {
+        HeaderComponent
+    },
+    data() {
+        return {
+            isLoggedIn: false,
+        }
+    },
+    created() {
+        if (window.Laravel.isLoggedin) {
+            this.isLoggedIn = true
+        }
+    },
+}
+</script>
