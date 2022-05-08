aa3202147371448212fc01799ccaa4511f05e7b8
Evgeniy Antipov <evantype@gmail.com>
2022-05-08 23:20:39 +0300

    апи маршруты

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,13 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
-Route::apiResource('users', UserController::class);
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'users', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'delete']);
+});
